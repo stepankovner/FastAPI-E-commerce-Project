9388448b278653f4c1ef63953de267fadc12bb8a
stepankovner <stepan_kovner@mail.ru>
2025-10-18 15:19:52 +0300

    Feature: Add Loguru logging and HTTP request middleware

--- fastapi_ecommerce/app/main.py
+++ fastapi_ecommerce/app/main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI, Request
-
+from fastapi.responses import JSONResponse
 from app.routers import categories, products, users, reviews
-
-from starlette.middleware.sessions import SessionMiddleware
-
+from uuid import uuid4
 import time
+
+from loguru import logger
 
 # Создаём приложение FastAPI
 app = FastAPI(
     title="FastAPI",
     version="0.1.0",
 )
-app.add_middleware(SessionMiddleware, secret_key="7UzGQS7woBazLUtVQJG39ywOP7J7lkPkB0UmDhMgBR8=")
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
 
 # Подключаем маршруты категорий и товаров
 app.include_router(categories.router)
@@ -29,7 +43,6 @@ async def root():
     return {"message": "Добро пожаловать в API интернет-магазина!"}
 
 
-
-
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue = True)
 
 
